716f4832acb8c9542422c0b74257aaa8035128fd
gen dee <jenniferdee86@gmail.com>
2022-11-19 16:29:40 +0000

    J Block Complete

--- BlockFiles/tilePieces/JBlock.cs
+++ BlockFiles/tilePieces/JBlock.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Tetris.BlockFiles.tilePieces
 {
-    class JBlock
+    class JBlock : Block
     {
+        protected override BlockPosition[][] Tiles => new BlockPosition[][] {
+            new BlockPosition[] {new(0, 0), new(1, 0), new(1, 1), new(1, 2)},
+            new BlockPosition[] {new(0, 1), new(0, 2), new(1, 1), new(2, 1)},
+            new BlockPosition[] {new(1, 0), new(1, 1), new(1, 2), new(2, 2)},
+            new BlockPosition[] {new(0, 1), new(1, 1), new(2, 1), new(2, 0)}
+        };
+
+        public override int id => 2;
+        protected override BlockPosition StartOffSet => new(0, 3);
     }
 }
