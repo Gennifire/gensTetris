3f2edf8381c5574173f7fb5c42f8b575a91c29df
gen dee <jenniferdee86@gmail.com>
2022-11-19 16:32:34 +0000

    S Block Complete

--- BlockFiles/tilePieces/SBlock.cs
+++ BlockFiles/tilePieces/SBlock.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Tetris.BlockFiles.tilePieces
 {
-    class SBlock
+    class SBlock : Block
     {
+        
+        protected override BlockPosition[][] Tiles => new BlockPosition[][] {
+            new BlockPosition[] { new(0,1), new(0,2), new(1,0), new(1,1) },
+            new BlockPosition[] { new(0,1), new(1,1), new(1,2), new(2,2) },
+            new BlockPosition[] { new(1,1), new(1,2), new(2,0), new(2,1) },
+            new BlockPosition[] { new(0,0), new(1,0), new(1,1), new(2,1) }
+        };
+
+        public override int id => 5;
+
+        protected override BlockPosition StartOffSet => new(0, 3);
     }
 }
