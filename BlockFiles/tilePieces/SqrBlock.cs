d5ceda3c8a3541934d09074df42bb49516327127
gen dee <jenniferdee86@gmail.com>
2022-11-19 16:25:27 +0000

    Sqr Block Complete

--- BlockFiles/tilePieces/SqrBlock.cs
+++ BlockFiles/tilePieces/SqrBlock.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Tetris.BlockFiles.tilePieces
 {
-    class SqrBlock
+    class SqrBlock : Block
     {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+       {
+            new BlockPosition[] { new(0,0), new(0,1), new(1,0), new(1,1) }
+       };
+
+        public override int id => 4;
+        protected override BlockPosition StartOffSet => new BlockPosition(0, 4);
+        protected override BlockPosition[][] Tiles => tiles;
     }
 }
