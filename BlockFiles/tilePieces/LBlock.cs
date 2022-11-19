47b1a379114f05c7d91ca20a473ee73b26b265c1
gen dee <jenniferdee86@gmail.com>
2022-11-19 16:31:16 +0000

    L Block Complete

--- BlockFiles/tilePieces/LBlock.cs
+++ BlockFiles/tilePieces/LBlock.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Tetris.BlockFiles.tilePieces
 {
-    class LBlock
+    class LBlock : Block
     {
+        
+
+        protected override BlockPosition[][] Tiles => new BlockPosition[][] {
+            new BlockPosition[] {new(0,2), new(1,0), new(1,1), new(1,2)},
+            new BlockPosition[] {new(0,1), new(1,1), new(2,1), new(2,2)},
+            new BlockPosition[] {new(1,0), new(1,1), new(1,2), new(2,0)},
+            new BlockPosition[] {new(0,0), new(0,1), new(1,1), new(2,1)}
+        };
+
+        public override int id => 3;
+
+        protected override BlockPosition StartOffSet => new(0, 3);
     }
 }
