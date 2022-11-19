a8cf1765f88f063ad44566c3669973de66b667e4
gen dee <jenniferdee86@gmail.com>
2022-11-19 15:36:48 +0000

    I Block Complete

--- BlockFiles/tilePieces/IBlock.cs
+++ BlockFiles/tilePieces/IBlock.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Tetris.BlockFiles.tilePieces
 {
-    class IBlock
+    class IBlock : Block
     {
+        private readonly BlockPosition[][] tiles = new BlockPosition[][]
+        {
+            new BlockPosition[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new BlockPosition[] { new(0,2), new(1,2), new(2,2), new(3,2) },
+            new BlockPosition[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new BlockPosition[] { new(1,0), new(1,1), new(1,2), new(1,3) }
+        };
+
+        public override int id => 1;
+        protected override BlockPosition StartOffSet => new BlockPosition(-1,3);
+        protected override BlockPosition[][] Tiles => tiles;
     }
 }
