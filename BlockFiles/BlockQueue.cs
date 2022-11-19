7758400d18b07dbc7282d85df4127523f6df77e2
gen dee <jenniferdee86@gmail.com>
2022-11-19 16:45:55 +0000

    Block Queue Class

---
+++ BlockFiles/BlockQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.BlockFiles.tilePieces
+{
+    public class BlockQueue
+    {
+        //create an array of each block type to call from
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new SBlock(),
+            new SqrBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random rand = new Random();
+
+        //get next block to preview in UI
+        public Block nextBlock { get; private set; }
+
+       
+        //method to get a random block from array
+        private Block RandomBlock()
+        {
+            return blocks[rand.Next(blocks.Length)];
+        }
+        //add the block to the queue
+        public BlockQueue()
+        {
+            nextBlock = RandomBlock();
+        }
+
+        public Block GetandUpdate()
+        {
+            Block block = nextBlock;
+
+            //returns a new block if the first block id match the previous block
+            do
+            {
+                nextBlock = RandomBlock();
+            }
+            while (block.id == nextBlock.id);
+
+            return block;
+        }
+    }
+}
